388625eea86dd9bb7cc03afacff4882436d40a8c
marcondesricarte@gmail.com <marcondesricarte@gmail.com@a6b468a2-4536-0857-d7c6-ee504784c9cd>
2014-07-06 14:04:49 +0000

    atualizando classes do projeto

--- Robocup/src/data/Ball.java
+++ Robocup/src/data/Ball.java
@@ -10,33 +10,37 @@ public class Ball {
 
 	// public static double CHARGE = -1;
 	
-	private float x;
-	private float y;
-	
+	private double x;
+	private double y;
+	private double confidence;
 	
 	public Ball(){
 		this.x = 0;
 		this.y = 0;
 	}
 
-
-	public float getX() {
+	public double getX() {
 		return x;
 	}
 
-
-	public void setX(float x) {
+	public void setX(double x) {
 		this.x = x;
 	}
 
-
-	public float getY() {
+	public double getY() {
 		return y;
 	}
 
-
-	public void setY(float y) {
+	public void setY(double y) {
 		this.y = y;
+	}
+
+	public double getConfidence() {
+		return confidence;
+	}
+
+	public void setConfidence(double confidence) {
+		this.confidence = confidence;
 	}
 	
 	
